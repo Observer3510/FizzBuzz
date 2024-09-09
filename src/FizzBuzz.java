42ef3908426ec19f3f0f9c15cf1807a1209ce3f8
Observer3510 <missjiayiwu@gmail.com>
2024-09-09 13:49:16 -0400

    refactored to extract helper method and added while loop version

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,32 +3,45 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // like the main part of python, or just a regular python file
 
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 100; i++) { // i++ is similar to i += 1 in Python
 
+            doFizzBuzz(i);
+        }
+
+        int i = 1;
+        while (i < 100)
+        {
+            i++;
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            doFizzBuzz(i);
+        }
+    }
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+    private static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-                System.out.println("Fizz Buzz");
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) { // && is equivalent to AND, if structure is the same
 
-            } else if (divisibleBy3) {
+            System.out.println("Fizz Buzz");
+            // System.out.println("Fizz Buzz"); is equivalent to print(a + '\n') for some string, a in Python
 
-                System.out.println("Fizz");
+        } else if (divisibleBy3) {
 
-            } else if (divisibleBy5) {
+            System.out.println("Fizz");
 
-                System.out.println("Buzz");
+        } else if (divisibleBy5) {
 
-            } else {
+            System.out.println("Buzz");
 
-                System.out.println(i);
+        } else {
 
-            }
+            System.out.println(i);
+
         }
     }
 }
